973c16aaf5021790c5348207b7a9471f10ecad42
rimoza <ridwan.maxamed1@gmail.com>
2022-11-10 07:09:53 -0800

    feat: create first route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,16 @@ require('dotenv').config();
 const express = require('express');
 //create instant of express app
 const app = express();
+
+//first express route
+app.get('/getRestaurants', (req,res) =>{
+    res.json({
+        status: "success",
+        name: "Carro edeg"
+    })
+})
+
+
 //listening port 
 const port = process.env.PORT || 3001;
 app.listen(port, () =>{
