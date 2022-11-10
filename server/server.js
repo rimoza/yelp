31a4ea3e71ddf88779d0ee4ead229027059cd5f4
rimoza <ridwan.maxamed1@gmail.com>
2022-11-10 06:18:13 -0800

    feat: setup express project

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 // import express
+const express = require('express');
 //create instant of express app
+const app = express();
 //listening port 
+
+app.listen(3000, () =>{
+    console.log('server is up and listening on port 3000')
+});
