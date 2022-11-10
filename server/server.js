933b28ff5b095627fdb8f129d684e8b1aba8900f
rimoza <ridwan.maxamed1@gmail.com>
2022-11-10 06:28:17 -0800

    feat: config dotenv file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+//configure .env file
+require('dotenv').config();
 // import express
 const express = require('express');
 //create instant of express app
 const app = express();
 //listening port 
-const port = 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () =>{
     console.log(`server is up and listening on port ${port}`)
 });
